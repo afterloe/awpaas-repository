a679075e9877a7e64b0b0a45c528734b47c2cbed
afterloe <lm6289511@gmail.com>
2018-04-04 09:47:42 +0800

    feat: 修复启动注册服务中心

--- src/main/kotlin/cn/awpaas/Launch.kt
+++ src/main/kotlin/cn/awpaas/Launch.kt
@@ -2,8 +2,14 @@ package cn.cityworks
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.web.reactive.config.EnableWebFlux
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableWebFlux
+//@EnableWebSecurity
 class Launch
 
 fun main(args: Array<String>) {

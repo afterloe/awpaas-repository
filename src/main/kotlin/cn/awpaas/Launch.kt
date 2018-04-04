11028128e65c5573c4eabb752f7e5e731b38ced2
afterloe <lm6289511@gmail.com>
2018-04-04 11:14:16 +0800

    feat: 数据安全配置处理

--- src/main/kotlin/cn/awpaas/Launch.kt
+++ src/main/kotlin/cn/awpaas/Launch.kt
@@ -3,13 +3,9 @@ package cn.cityworks
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.web.reactive.config.EnableWebFlux
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableWebFlux
-//@EnableWebSecurity
 class Launch
 
 fun main(args: Array<String>) {

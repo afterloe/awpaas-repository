195cd3d7cd42123093746292d55c2284254787ed
afterloe <lm6289511@gmail.com>
2018-04-07 23:05:14 +0800

    feat: 切换为go模式

---
+++ routers/Index.go
@@ -0,0 +1,13 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"./images"
+)
+
+func Execute(route *gin.RouterGroup) {
+	route.GET("/", Home)
+	route.GET("/images", images.ListImage)
+	route.POST("/images", Home)
+	route.POST("/images/load", Home)
+}

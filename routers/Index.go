e8dca3e056325f8b914c2951c7861582fe075dfc
afterloe <605728727@qq.com>
2018-12-23 00:25:29 +0800

    feat: 优化查询配置

--- routers/Index.go
+++ routers/Index.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 /**
@@ -10,4 +11,20 @@ import (
 func Execute(route *gin.RouterGroup) {
 	route.GET("/remote/repository", RemoteList)
 	route.GET("/warehouse/list", WarehouseList)
+}
+
+func pageCondition(context *gin.Context) (int, int) {
+	begin, err := strconv.Atoi(context.DefaultQuery("bg", "0"))
+	if nil != err {
+		begin = 0
+	}
+	end, err := strconv.Atoi(context.DefaultQuery("ed", "10"))
+	if nil != err {
+		end = 10
+	}
+	limit := end - begin
+	if 0 >= limit {
+		limit = 10
+	}
+	return begin, limit
 }
--- routers/Warehouse.go
+++ routers/Warehouse.go
@@ -9,22 +9,7 @@ import (
 )
 
 func WarehouseList(context *gin.Context) {
-	var (
-		begin_str = context.DefaultQuery("bg", "0")
-		end_str = context.DefaultQuery("ed", "10")
-	)
-	begin, err := strconv.Atoi(begin_str)
-	if nil != err {
-		begin = 0
-	}
-	end, err := strconv.Atoi(end_str)
-	if nil != err {
-		end = 10
-	}
-	limit := end - begin
-	if 0 >= limit {
-		limit = 10
-	}
+	begin, limit := pageCondition(context)
 	reply  := warehouse.GetList(strconv.Itoa(begin), strconv.Itoa(limit))
 	context.JSON(http.StatusOK, util.Success(reply))
 }

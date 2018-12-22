d118b2c029388a26d899d650a5a5a9748dbfcde7
afterloe <605728727@qq.com>
2018-12-22 17:23:45 +0800

    feat: 信息升级

--- test/HttpClient_test.go
+++ test/HttpClient_test.go
@@ -3,26 +3,8 @@ package test
 import (
 	"testing"
 	"../integrate/soaClient"
-	"encoding/json"
 	"fmt"
 )
-
-func FormatToStruct(chunk *string) (map[string]interface{}, error){
-	rep := make(map[string]interface{})
-	err := json.Unmarshal([]byte(*chunk), &rep) // 使用这种方式来转换复杂json
-	if nil != err {
-		return nil, err
-	}
-	return rep, nil
-}
-
-func FormatToString(vol interface{}) (string, error){
-	buf, err := json.Marshal(vol)
-	if nil != err {
-		return "",err
-	}
-	return string(buf),nil
-}
 
 func Test_HttpClient_demo(t *testing.T) {
 	reply, err := soaClient.Call("GET",  "192.168.3.21:180", "/v2/_catalog", nil, nil)

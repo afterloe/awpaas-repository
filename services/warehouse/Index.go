a5eb531f3934a0c7d108a52e3b49c019f3626582
afterloe <605728727@qq.com>
2018-12-23 22:46:07 +0800

    feat: 远程联调，补充测试检测异常

--- services/warehouse/Index.go
+++ services/warehouse/Index.go
@@ -3,10 +3,14 @@ package warehouse
 import (
 	"../../integrate/soaClient"
 	"../../config"
+	"../../exceptions"
+	"fmt"
+	"reflect"
 )
 
 var (
 	addr,port  string
+	db string
 )
 
 func init() {
@@ -14,7 +18,50 @@ func init() {
 	addr = config.GetByTarget(db, "addr").(string)
 	port = config.GetByTarget(db, "port").(string)
 	addr += ":" + port
+	db = "registry"
 }
+
+type Module struct {
+	Name, Group, Remarks, Version, Fid string
+	_id string
+}
+
+/**
+	参数检测
+*/
+func (this *Module) Check(args ...string) error {
+	value := reflect.ValueOf(*this)
+	for _, arg := range args {
+		v := value.FieldByName(arg)
+		if !v.IsValid() {
+			break
+		}
+		if "" == v.Interface() {
+			return &exceptions.Error{Msg: "lack param " + arg, Code: 400}
+		}
+	}
+
+	return nil
+}
+
+/**
+	发送至远程
+*/
+func (this *Module) AppendToRemote() (map[string]interface{}, error) {
+	reply, _ := soaClient.Call("GET", addr, "/_uuids?count=1", nil, nil)
+	id := reply["uuids"].([]interface{})[0]
+	this._id = id.(string)
+	save:
+	reply, _ = soaClient.Call("PUT", addr, fmt.Sprintf("/%s/%v", addr, id),
+		soaClient.GeneratorBody(this), soaClient.GeneratorPostHeader())
+	// 如果数据库不存在，则创建
+	if "not_found" == reply["error"]{
+		soaClient.Call("PUT", addr, db, nil, nil)
+		goto save
+	}
+	return reply, nil
+}
+
 /**
 	获取包列表
 */
@@ -24,7 +71,7 @@ func GetList(skip, limit string) []interface{} {
 		"limit": limit,
 		"include_docs": "true",
 	})
-	reply, err := soaClient.Call("GET", addr, "/registry/_all_docs?" + params, nil, nil)
+	reply, err := soaClient.Call("GET", addr, fmt.Sprintf("/%s/%s?%s", db, "_all_docs", params), nil, nil)
 	var list = make([]interface{}, 0)
 	if nil != err {
 		return list

b5386cd6589f4dcf7b44c3a9ee23f91739f2140d
afterloe <lm6289511@gmail.com>
2018-12-25 09:03:06 +0800

    feat: couchdb联调

--- test/QueryFromRemote_test.go
+++ test/QueryFromRemote_test.go
@@ -5,14 +5,26 @@ import (
 	"../integrate/soaClient"
 )
 
+var (
+	host, dbName string
+	username, password string
+)
+
+func init() {
+	host = "mine:5984"
+	dbName = "file-system"
+	username = "ascs"
+	password = "ascs.tech"
+}
+
 func Test_QueryToCouchDB(t *testing.T) {
-	reply, err := soaClient.Call("GET",  "192.168.3.21:5984", "/registry/_all_docs?include_docs=true&limit=10&skip=10", nil, nil)
+	reply, err := soaClient.Call("GET",  host, "/hyoertable/_all_docs?include_docs=true&limit=10&skip=0", nil, nil)
 	if nil != err {
 		t.Error(err)
 	}
 	t.Log(reply)
 	for _, v := range reply["rows"].([]interface{}) {
 		doc := ((v.(map[string]interface{}))["doc"]).(map[string]interface{})
-		t.Log(doc["name"])
+		t.Log(doc)
 	}
 }

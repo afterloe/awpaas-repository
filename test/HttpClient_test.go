466fb5a8aaf8e3aa69dba723e074903377ac5ce3
afterloe <605728727@qq.com>
2018-12-27 23:47:23 +0800

    feat: couchdb sdk可直接创建数据项

--- test/HttpClient_test.go
+++ test/HttpClient_test.go
@@ -6,18 +6,24 @@ import (
 )
 
 func Test_HttpClient_demo(t *testing.T) {
-	//flag, err := couchdb.Login()
-	//if nil != err {
-	//	t.Error(err)
-	//	return
-	//}
-	//t.Log(flag)
-	reply, err := couchdb.CreateDB("demo")
+	reply, err := couchdb.Create("demoi", map[string]interface{}{
+		"name": "afterloe",
+		"age": 6,
+		"sex": "小男孩",
+	})
 	if nil != err {
 		t.Error(reply)
 	}
 	t.Log(reply)
 }
+
+//func Test_HttpClient_demo(t *testing.T) {
+//	reply, err := couchdb.CreateDB("demo")
+//	if nil != err {
+//		t.Error(reply)
+//	}
+//	t.Log(reply)
+//}
 
 //func Test_HttpClient_demo(t *testing.T) {
 //	flag, err := couchdb.Login()

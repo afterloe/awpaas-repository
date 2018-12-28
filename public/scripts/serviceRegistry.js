b6e7ddff1bcf63960aff02e2852966770bc0c08e
afterloe <lm6289511@gmail.com>
2018-12-28 16:58:54 +0800

    feat: 前端可视化页面加入

---
+++ public/scripts/serviceRegistry.js
@@ -0,0 +1,241 @@
+"use strict";
+
+const mapToList = data => {
+    const arr = [];
+    Object.keys(data).map(it => arr.push({serviceName: it, addr: data[it]}));
+    return arr;
+};
+
+const serviceRegistryURL = "/manager/v1/serviceMap";
+
+class ServiceRegistry extends React.Component {
+    constructor(props) {
+        super(props);
+        this.showMenu = this.showMenu.bind(this);
+        this.itemClick = this.itemClick.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.appendItemToRemote = this.appendItemToRemote.bind(this);
+        this.renderServiceMap = this.renderServiceMap.bind(this);
+        this.renderMsgAlert = this.renderMsgAlert.bind(this);
+        this.openDelWindow = this.openDelWindow.bind(this);
+        this.deleteToRemote = this.deleteToRemote.bind(this);
+        this.openModifyWindow = this.openModifyWindow.bind(this);
+        this.modifyItemToRemote = this.modifyItemToRemote.bind(this);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        const that = this;
+        getListFromRemote(serviceRegistryURL).then(data => {
+            that.setState({serviceList: mapToList(data)}); // 初始化数据
+        }).catch(error => {
+            that.setState({serviceList: [], msg: {type: "error", context: error}});
+        });
+    }
+
+    static closeMenu(dom) {
+        const flag = dom.getAttribute("data-flag");
+        if ("f" === flag) {
+            dom.setAttribute("data-flag", "t");
+            dom.setAttribute("class", "dropdown-menu show")
+        } else {
+            dom.setAttribute("data-flag", "f");
+            dom.setAttribute("class", "dropdown-menu")
+        }
+    }
+
+    modifyItemToRemote(data, flag) {
+        const [
+            {serviceList= [], msg= {}},
+            {serviceName, addr},
+            that
+        ] = [
+            this.state,
+            data,
+            this
+        ];
+        if (!flag) return ;
+        const index = serviceList.findIndex(item => {
+            return item.serviceName == serviceName;
+        });
+        if (-1 === index) {
+            Object.assign(msg, {type: "error", context: "服务映射不存在于注册中心..."});
+            that.setState({msg});
+            return ;
+        }
+        appendToRemote({serviceName:serviceName, serviceAddr:addr}, serviceRegistryURL).then(() => {
+            serviceList[index] = data;
+            Object.assign(msg, {type: "success", context: "服务修改成功..."});
+            that.setState({serviceList, msg});
+        }).catch(error => {
+            Object.assign(msg, {type: "error", context: error});
+            that.setState({msg});
+        });
+    }
+
+    appendItemToRemote(data) {
+        const [
+            {serviceList= [], msg= {}},
+            {serviceName, addr},
+            that
+        ] = [
+            this.state,
+            data,
+            this
+        ];
+        const index = serviceList.findIndex(item => {
+            return item.serviceName == serviceName;
+        });
+        if (-1 !== index) {
+            Object.assign(msg, {type: "error", context: "服务已存在..."});
+            that.setState({msg});
+            return ;
+        }
+        newToRemote({serviceName, serviceAddr:addr}, serviceRegistryURL).then(() => {
+            serviceList.push({serviceName, addr});
+            Object.assign(msg, {type: "success", context: "服务注册成功..."});
+            that.setState({serviceList, msg});
+        }).catch(error => {
+            Object.assign(msg, {type: "error", context: error});
+            that.setState({msg});
+        });
+    }
+
+    openModal() {
+        ReactDOM.render(<ModalWindow_editService title={"添加服务注册信息"} callback={this.appendItemToRemote} />, document.getElementById("modal"));
+    }
+
+    deleteToRemote(index) {
+        const that = this;
+        const {serviceList= [], msg= {}} = this.state;
+        if (-1 === index) {
+            Object.assign(msg, {type: "error", context: "删除失败:服务未注册..."}); 
+            return ;
+        }
+        const service = serviceList[index];
+        deleteFromRemote({serviceName: service.serviceName}, serviceRegistryURL).then(() => {
+            serviceList.splice(index, 1)
+            Object.assign(msg, {type: "success", context: "删除成功..."});
+            that.setState({msg, serviceList});
+        }).catch(error => {
+            Object.assign(msg, {type: "error", context: error});
+            that.setState({msg, serviceList});
+        });
+    }
+
+    openModifyWindow(index) {
+        const {serviceList = []} = this.state;
+        const service = serviceList[index];
+        ReactDOM.render(<ModalWindow_editService title={"修改服务注册信息"} serviceName={service.serviceName} addr={service.addr} callback={this.modifyItemToRemote} />, document.getElementById("modal"));
+    }
+
+    openDelWindow(index) {
+        const {serviceList = []} = this.state;
+        const service = serviceList[index];
+        const context = `确认删除 \t ${service.serviceName} \t ? \r\n 映射地址为 ${service.addr}`;
+        ReactDOM.render(<ModalWindow_alert title={"删除此项"} context={context} value={index} callback={this.deleteToRemote}/>, document.getElementById("modal"));
+    }
+
+    itemClick(event) {
+        const item = event.target;
+        const [
+            cmd,
+            index,
+        ]= [
+            item.getAttribute("data-cmd") || "", 
+            item.parentNode.getAttribute("data-index"),
+        ];
+        if ("" === cmd) {
+            ServiceRegistry.closeMenu(item)
+        }
+        ServiceRegistry.closeMenu(item.parentNode);
+        switch (cmd) {
+            case "del": 
+                this.openDelWindow(index);
+                return;
+            case "modify":
+                this.openModifyWindow(index);
+                return;
+            default:
+                return;
+        }
+    }
+
+    renderMsgAlert() {
+        const {msg = {}} = this.state;
+        const {type, context = ""} = msg;
+        return "" === context? "": <MsgAlert type= {type} msg= {context} closeAlert= {()=>this.setState({msg: {}})}/>
+    }
+
+    showMenu(event) {
+        const menu = event.currentTarget.nextSibling;
+        ServiceRegistry.closeMenu(menu);
+    }
+
+    renderServiceMap() {
+        const {serviceList = []} = this.state;
+        return 0 === serviceList.length? ( 
+        <div className="media text-muted pt-3">
+            暂无数据!
+        </div>): serviceList.map((item, index) => (
+            <div className="media text-muted pt-3">
+                <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                    <div className="d-flex justify-content-between align-items-center w-100">
+                        <strong className="text-gray-dark">
+                            <span className="badge badge-success">正常</span> {item.serviceName}
+                        </strong>
+                        <div className="cont-btn btn-group show" role="group">
+                            <svg onClick={this.showMenu} xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" 
+                                stroke-linecap="round" stroke-linejoin="round" className="feather feather-more-vertical">
+                                <circle cx="12" cy="12" r="1"></circle>
+                                <circle cx="12" cy="5" r="1"></circle>
+                                <circle cx="12" cy="19" r="1"></circle>
+                            </svg>
+                            <div className="dropdown-menu" data-index={index} data-flag="f" onClick={this.itemClick}>
+                                <span className="dropdown-item" data-cmd="del">删除</span>
+                                <span className="dropdown-item" data-cmd="modify">修改</span>
+                                <span className="dropdown-item" data-cmd="detail">详情</span>
+                                <span className="dropdown-item" data-cmd="rely">依赖关系</span>
+                            </div>
+                        </div>
+                    </div>
+                    <span className="d-block detail">
+                        <span>map to: <strong>{item.addr}</strong></span>
+                        <span>register time: <strong>{item.registryTime || new Date().toLocaleString()}</strong></span>
+                    </span>
+                </div>
+            </div>
+        ));
+    }
+
+    render() {
+        return (
+            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+                <div
+                    className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 className="h2">服务注册列表</h1>
+                    <div className="btn-toolbar mb-2 mb-md-0">
+                        <div className="btn-group mr-2">
+                            <button className="btn btn-sm btn-outline-secondary cont-btn" onClick={this.openModal}>
+                                <embed src="images/upload-cloud.svg" width="16" height="16" type="image/svg+xml"/>
+                                <span>手动注册</span>
+                            </button>
+                            <button className="btn btn-sm btn-outline-secondary cont-btn">
+                                <embed src="images/aperture.svg" width="16" height="16" type="image/svg+xml"/>
+                                <span>扫描</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="my-3 p-3 bg-white rounded shadow-sm m-cent">
+                    <h6 className="border-bottom d-flex justify-content-between align-items-center">
+                    </h6>
+                        {this.renderMsgAlert()}
+                        {this.renderServiceMap()}
+                    <div id="modal"></div>
+                </div>
+            </main>
+        );
+    }
+}
---
+++ public/scripts/whiteManager.js
@@ -0,0 +1,215 @@
+"use strict";
+
+const generatorModify= async (old, n, path) => {
+    await deleteFromRemote(old, path);
+    await appendToRemote(n, path);
+};
+
+const whiteManagerURL = "/manager/v1/whiteList";
+
+class WhiteManager extends React.Component {
+    constructor(props) {
+        super(props);
+        this.appendItem = this.appendItem.bind(this);
+        this.editItem = this.editItem.bind(this);
+        this.syncToRemote = this.syncToRemote.bind(this);
+        this.renderMsgAlert = this.renderMsgAlert.bind(this);
+        this.closeMsgAlert = this.closeMsgAlert.bind(this);
+        this.appendItemToRemote = this.appendItemToRemote.bind(this);
+        this.modifyToRemote = this.modifyToRemote.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+        this.deleteToRemote = this.deleteToRemote.bind(this);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        const that = this;
+        getListFromRemote(whiteManagerURL).then(data => {
+            that.setState({list: data}); // 初始化数据
+        }).catch(error => {
+            that.setState({list: [], msg: {type: "error", context: error}});
+        });
+    }
+
+    deleteToRemote(data) {
+        const that = this;
+        const {msg = {}, list} = that.state;
+        const index = list.findIndex(it => data === it);
+        if (-1 !== index) {
+            deleteFromRemote({item: data}, whiteManagerURL).then(() => {
+                list.splice(index, 1);
+                Object.assign(msg, {type: "success", context: "删除成功..."});
+                that.setState({msg, list});
+            }).catch(error => {
+                Object.assign(msg, {type: "error", context: error});
+                that.setState({msg});
+            });
+        } else {
+            Object.assign(msg, {type: "error", context: "删除失败..."});
+            that.setState({msg});
+        }
+
+    }
+
+    modifyToRemote(data, flag, oldData) {
+        if (!flag) return;
+        const that = this;
+        const {msg = {}, list} = that.state;
+        if (data === oldData) {
+            Object.assign(msg, {type: "error", context: "未被修改..."});
+            that.setState({msg});
+            return;
+        }
+        const index = list.findIndex(it => oldData === it);
+        if (-1 === index) {
+            Object.assign(msg, {type: "error", context: "数据已被删除..."});
+            that.setState({msg});
+            return;
+        }
+        generatorModify({item: oldData}, {item: data}, whiteManagerURL).then(() => {
+            list[index] = data;
+            Object.assign(msg, {type: "success", context: "修改成功..."});
+            that.setState({msg, list});
+        }).catch(error => {
+            Object.assign(msg, {type: "error", context: error});
+            that.setState({msg});
+        });
+    }
+
+    appendItemToRemote(data, flag) {
+        if (!flag) return ;
+        const that = this;
+        const {msg = {}, list} = that.state;
+        const index = list.findIndex(it => data === it);
+        if (-1 === index) {
+            appendToRemote({item: data}, whiteManagerURL).then(() => {
+                list.push(data);
+                Object.assign(msg, {type: "success", context: "保存成功..."});
+                that.setState({msg, list});
+            }).catch(error => {
+                Object.assign(msg, {type: "error", context: error});
+                this.setState({msg});
+            });
+        } else {
+            Object.assign(msg, {type: "error", context: "失败：元素已存在..."});
+            this.setState({msg});
+        }
+
+    }
+
+    deleteItem(event) {
+        const content = event.currentTarget.parentNode.previousSibling.textContent;
+        const context = `确认删除 \t ${content} \t ?`;
+        ReactDOM.render(<ModalWindow_alert title={"删除此项"} context={context} value={content} callback={this.deleteToRemote}/>, document.getElementById("modal"));
+    }
+
+    editItem(event) {
+        const content = event.currentTarget.parentNode.previousSibling.textContent;
+        ReactDOM.render(<ModalWindow title={"修改记录"} itemName={"白名单"} value={content} callback={this.modifyToRemote}/>, document.getElementById("modal"));
+    }
+
+    appendItem() {
+        ReactDOM.render(<ModalWindow title={"添加记录"} itemName={"白名单"} callback={this.appendItemToRemote} />, document.getElementById("modal"));
+    }
+
+    renderMsgAlert(msg) {
+        const {type, context = ""} = msg;
+        return "" === context? "": <MsgAlert type= {type} msg= {context} closeAlert= {this.closeMsgAlert}/>
+    }
+
+    closeMsgAlert() {
+        this.setState({msg: {}})
+    }
+
+    syncToRemote() {
+        const that = this;
+        that.setState({msg: {type: "info", context: "同步中..."}});
+        getListFromRemote(whiteManagerURL).then(data => {
+            that.setState({list: data}); // 初始化数据
+        }).catch(error => {
+            that.setState({list: [], msg: {type: "error", context: error}});
+        });
+    }
+
+    renderList(list = []) {
+        return list.map(it => (
+            <div className="media text-muted pt-3">
+                <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                    <div className="d-flex justify-content-between align-items-center w-100">
+                        <strong className="text-gray-dark">{it}</strong>
+                        <span>
+                            <span className="cont-btn" onClick={this.editItem}>
+                                <embed src="images/edit.svg" width="16" height="16" type="image/svg+xml"/>
+                                <span>修改</span>
+                            </span>
+                            <span className="cont-btn" onClick={this.deleteItem}>
+                                <embed src="images/trash.svg" width="16" height="16" type="image/svg+xml"/>
+                                <span>删除</span>
+                            </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        ));
+    }
+
+    render() {
+        const {msg = {}, list = []} = this.state;
+        return (
+            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+                <div
+                    className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 className="h2">网关白名单</h1>
+                </div>
+                {this.renderMsgAlert(msg)}
+                <div class="my-3 p-3 bg-white rounded shadow-sm m-cent">
+                    <h6 class="border-bottom border-gray pb-2 mb-0">拦截信息</h6>
+                    <div class="media text-muted pt-3">
+                        <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                            <div class="d-flex justify-content-between align-items-center w-100">
+                                <strong class="text-gray-dark">请求头</strong>
+                                <span class="cont-btn">
+                                    <embed src="images/settings.svg" width="16" height="16" type="image/svg+xml"/>
+                                    <span>设置</span>
+                                </span>
+                            </div>
+                            <span class="d-block">access-token</span>
+                        </div>
+                    </div>
+                    <div class="media text-muted pt-3">
+                        <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                            <div class="d-flex justify-content-between align-items-center w-100">
+                                <strong class="text-gray-dark">启用拦截</strong>
+                                <span class="cont-btn">
+                                    <span>ON</span>
+                                </span>
+                            </div>
+                            <span class="d-block">未携带请求头的链接进行自动拦截</span>
+                        </div>
+                    </div>
+                    <small class="d-block text-right mt-3 cont-btn" onClick={this.syncToRemote}>
+                        <embed src="images/refresh-cw.svg" width="16" height="16" type="image/svg+xml"/>
+                        <span>同步</span>
+                    </small>
+                </div>
+                <div class="my-3 p-3 bg-white rounded shadow-sm m-cent">
+                    <h6 class="border-bottom d-flex justify-content-between align-items-center">
+                        <span class="d-block">列表</span>
+                        <small class="d-block text-right mt-3 mb-3">
+                            <span class="cont-btn" onClick={this.syncToRemote}>
+                                <embed src="images/refresh-cw.svg" width="16" height="16" type="image/svg+xml"/>
+                                <span>同步</span>
+                            </span>
+                            <span class="cont-btn" onClick={this.appendItem}>
+                                <embed src="images/plus-circle.svg" width="16" height="16" type="image/svg+xml"/>
+                                <span>添加</span>
+                            </span>
+                        </small>
+                    </h6>
+                    {this.renderList(list)}
+                </div>
+                <div id="modal"></div>
+            </main>
+        );
+    }
+}

a3a6fcb09dafc11bdb18f711e051f5cc0cdc83cc
afterloe <lm6289511@gmail.com>
2018-12-29 09:35:23 +0800

    feat:前端页面优化

--- public/scripts/warehouse.js
+++ public/scripts/warehouse.js
@@ -5,9 +5,9 @@ const generatorModify= async (old, n, path) => {
     await appendToRemote(n, path);
 };
 
-const whiteManagerURL = "/manager/v1/whiteList";
+const warehouseURL = "/gateway/repository/v1/warehouse";
 
-class WhiteManager extends React.Component {
+class Warehouse extends React.Component {
     constructor(props) {
         super(props);
         this.appendItem = this.appendItem.bind(this);
@@ -24,7 +24,7 @@ class WhiteManager extends React.Component {
 
     componentDidMount() {
         const that = this;
-        getListFromRemote(whiteManagerURL).then(data => {
+        getListFromRemote(warehouseURL).then(data => {
             that.setState({list: data}); // 初始化数据
         }).catch(error => {
             that.setState({list: [], msg: {type: "error", context: error}});
@@ -36,7 +36,7 @@ class WhiteManager extends React.Component {
         const {msg = {}, list} = that.state;
         const index = list.findIndex(it => data === it);
         if (-1 !== index) {
-            deleteFromRemote({item: data}, whiteManagerURL).then(() => {
+            deleteFromRemote({item: data}, warehouseURL).then(() => {
                 list.splice(index, 1);
                 Object.assign(msg, {type: "success", context: "删除成功..."});
                 that.setState({msg, list});
@@ -66,7 +66,7 @@ class WhiteManager extends React.Component {
             that.setState({msg});
             return;
         }
-        generatorModify({item: oldData}, {item: data}, whiteManagerURL).then(() => {
+        generatorModify({item: oldData}, {item: data}, warehouseURL).then(() => {
             list[index] = data;
             Object.assign(msg, {type: "success", context: "修改成功..."});
             that.setState({msg, list});
@@ -82,7 +82,7 @@ class WhiteManager extends React.Component {
         const {msg = {}, list} = that.state;
         const index = list.findIndex(it => data === it);
         if (-1 === index) {
-            appendToRemote({item: data}, whiteManagerURL).then(() => {
+            appendToRemote({item: data}, warehouseURL).then(() => {
                 list.push(data);
                 Object.assign(msg, {type: "success", context: "保存成功..."});
                 that.setState({msg, list});
@@ -159,42 +159,14 @@ class WhiteManager extends React.Component {
             <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
                 <div
                     className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 className="h2">网关白名单</h1>
+                    <h1 className="h2">镜像仓库</h1>
                 </div>
                 {this.renderMsgAlert(msg)}
                 <div class="my-3 p-3 bg-white rounded shadow-sm m-cent">
-                    <h6 class="border-bottom border-gray pb-2 mb-0">拦截信息</h6>
-                    <div class="media text-muted pt-3">
-                        <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                            <div class="d-flex justify-content-between align-items-center w-100">
-                                <strong class="text-gray-dark">请求头</strong>
-                                <span class="cont-btn">
-                                    <embed src="images/settings.svg" width="16" height="16" type="image/svg+xml"/>
-                                    <span>设置</span>
-                                </span>
-                            </div>
-                            <span class="d-block">access-token</span>
-                        </div>
-                    </div>
-                    <div class="media text-muted pt-3">
-                        <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                            <div class="d-flex justify-content-between align-items-center w-100">
-                                <strong class="text-gray-dark">启用拦截</strong>
-                                <span class="cont-btn">
-                                    <span>ON</span>
-                                </span>
-                            </div>
-                            <span class="d-block">未携带请求头的链接进行自动拦截</span>
-                        </div>
-                    </div>
-                    <small class="d-block text-right mt-3 cont-btn" onClick={this.syncToRemote}>
-                        <embed src="images/refresh-cw.svg" width="16" height="16" type="image/svg+xml"/>
-                        <span>同步</span>
-                    </small>
-                </div>
-                <div class="my-3 p-3 bg-white rounded shadow-sm m-cent">
                     <h6 class="border-bottom d-flex justify-content-between align-items-center">
-                        <span class="d-block">列表</span>
+                        <span class="d-block input-container">
+                            <input className="input" type="text" placeholder="过滤" aria-label="过滤"/>
+                        </span>
                         <small class="d-block text-right mt-3 mb-3">
                             <span class="cont-btn" onClick={this.syncToRemote}>
                                 <embed src="images/refresh-cw.svg" width="16" height="16" type="image/svg+xml"/>
@@ -207,6 +179,53 @@ class WhiteManager extends React.Component {
                         </small>
                     </h6>
                     {this.renderList(list)}
+                    <div className="media text-muted pt-3 item">
+                        <div className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                            <div className="d-flex justify-content-between align-items-center w-100">
+                                <strong className="text-gray-dark">"ss</strong>
+                                <span>
+                                    <span className="cont-btn" onClick={this.editItem}>
+                                        <embed src="images/edit.svg" width="16" height="16" type="image/svg+xml"/>
+                                        <span>详情</span>
+                                    </span>
+                                    <span className="cont-btn" onClick={this.editItem}>
+                                        <embed src="images/edit.svg" width="16" height="16" type="image/svg+xml"/>
+                                        <span>修改</span>
+                                    </span>
+                                    <span className="cont-btn" onClick={this.deleteItem}>
+                                        <embed src="images/trash.svg" width="16" height="16" type="image/svg+xml"/>
+                                        <span>删除</span>
+                                    </span>
+                                </span>
+                            </div>
+                            <span className="d-block">未携带请求头的链接进行自动拦截</span>
+                        </div>
+                    </div>
+
+                    <div className="media text-muted pt-3">
+                        <div className={"media-body pb-3 mb-0 small lh-125 border-bottom border-gray"}>
+                            <div className="d-flex justify-content-between align-items-center w-100">
+                                <strong className="text-gray-dark"></strong>
+                                <nav aria-label="Page navigation example">
+                                    <ul className="pagination">
+                                        <li className="page-item">
+                                            <a className="page-link" href="#" aria-label="Previous">
+                                                <span aria-hidden="true">&laquo;</span>
+                                            </a>
+                                        </li>
+                                        <li className="page-item"><a className="page-link" href="#">1</a></li>
+                                        <li className="page-item"><a className="page-link" href="#">2</a></li>
+                                        <li className="page-item"><a className="page-link" href="#">3</a></li>
+                                        <li className="page-item">
+                                            <a className="page-link" href="#" aria-label="Next">
+                                                <span aria-hidden="true">&raquo;</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div id="modal"></div>
             </main>

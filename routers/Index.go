b1bdc43b99463e88d62134476e9fbd9d2d95cefc
afterloe <lm6289511@gmail.com>
2019-01-17 18:07:52 +0800

    feat: 参数调整

--- routers/Index.go
+++ routers/Index.go
@@ -31,7 +31,6 @@ func Execute(route *gin.RouterGroup) {
 	route.PUT("/ci/warehouse/:key", CmdBuilder) // 构建 - 创建构建命令
 	route.DELETE("/ci/warehouse/:key") // 构建 - 删除构建命令
 	route.POST("/ci/warehouse/:key", CmdCi) // 构建 - 执行构建命令
-	route.GET("/ci/report", CmdReport)
 }
 
 /**
